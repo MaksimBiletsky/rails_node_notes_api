39b911b059f80a8aa690fc344c0709fc3bb3d300
just_a_sea_f <maximbiletsky@gmail.com>
2024-07-15 21:58:28 +0300

    Add Note model and endpoints

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,50 @@
+class NotesController < ApplicationController
+  before_action :set_note, only: [:show, :update, :destroy]
+
+  def index
+    @notes = Note.all
+    render json: @notes
+  end
+
+  def show
+    render json: @note
+  end
+
+  def create
+    @note = Note.new(note_params)
+
+    if @note.save
+      render json: @note, status: :created
+    else
+      render json: @note.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @note.update(note_params)
+      render json: @note
+    else
+      render json: @note.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @note.destroy
+    head :no_content
+  end
+
+  def search
+    @notes = Note.where("title LIKE ? OR content LIKE ?", "%#{params[:query]}%", "%#{params[:query]}%")
+    render json: @notes
+  end
+
+  private
+
+  def set_note
+    @note = Note.find(params[:id])
+  end
+
+  def note_params
+    params.require(:note).permit(:title, :content)
+  end
+end
