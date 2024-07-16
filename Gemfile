0e599c97fc688d79b11112aea8b42d65b01ea001
just_a_sea_f <maximbiletsky@gmail.com>
2024-07-16 20:06:03 +0300

    Node.js server to test Rails server

--- Gemfile
+++ Gemfile
@@ -46,6 +46,7 @@ gem "dotenv"
 
 gem 'resque'
 gem 'resque-scheduler'
+gem 'rack-cors'
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
---
+++ nodejs-rails-api-demo/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = 3001;
+
+app.use(express.json());
+
+const railsApiBaseUrl = 'http://localhost:3000';
+
+app.get('/notes', async (req, res) => {
+  try {
+    const response = await axios.get(`${railsApiBaseUrl}/notes`);
+    res.json(response.data);
+  } catch (error) {
+    res.status(error.response.status).json({ error: error.message });
+  }
+});
+
+app.post('/notes', async (req, res) => {
+  try {
+    const newNote = req.body;
+    const response = await axios.post(`${railsApiBaseUrl}/notes`, newNote);
+    res.json(response.data);
+  } catch (error) {
+    res.status(error.response.status).json({ error: error.message });
+  }
+});
+
+app.put('/notes/:id', async (req, res) => {
+  try {
+    const updatedNote = req.body;
+    const noteId = req.params.id;
+    const response = await axios.put(`${railsApiBaseUrl}/notes/${noteId}`, updatedNote);
+    res.json(response.data);
+  } catch (error) {
+    res.status(error.response.status).json({ error: error.message });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Node.js server listening at http://localhost:${port}`);
+});
