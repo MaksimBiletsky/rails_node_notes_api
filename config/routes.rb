b7badb0891a74b5f6a5bacf25dfb96a066b66597
just_a_sea_f <maximbiletsky@gmail.com>
2024-07-16 09:58:21 +0300

    Resque endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
+require "resque/server"
+require 'resque/scheduler/server'
+
 Rails.application.routes.draw do
   resources :notes do
     get "search", on: :collection
     post "import_from_external_service", on: :collection
   end
+
+  mount Resque::Server.new, at: "/jobs"
 end
